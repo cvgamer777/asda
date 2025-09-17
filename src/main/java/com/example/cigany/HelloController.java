a3a563c0ba03a7c143b179050c68fec87726ae9e
Tarjanyi Levente <tarjanyi.levi@gmail.com>
2025-09-17 13:17:16 +0200

    SZAR LETT

--- src/main/java/com/example/cigany/HelloController.java
+++ src/main/java/com/example/cigany/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.cigany;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Pane pane_alakzat;
+    private ImageView imageview_alakzat;
+    private ListView Listview;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onPriosSelected(ActionEvent actionEvent) {
+        pane_alakzat.setBackground(new Background(new BackgroundFill(Color.RED,null,null)));
+
     }
 }
